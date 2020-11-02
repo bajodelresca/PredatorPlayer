d9a1bc638f6cc5d2940a36c229e66d2c72fb1116
Alberto Ruiz <ruizserranoalberto5@gmail.com>
2020-11-02 14:55:18 +0100

    Interdaz DAO implementada

--- src/main/java/DAO/ArtistaDAO.java
+++ src/main/java/DAO/ArtistaDAO.java
@@ -5,10 +5,32 @@
  */
 package DAO;
 
+import java.util.List;
+
 /**
  *
  * @author Alberto343
  */
-public class ArtistaDAO {
+public class ArtistaDAO implements DAO{
+
+    @Override
+    public void insert(Object a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void edit(Object a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void remove(Object a) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
